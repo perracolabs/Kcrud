2c0f42d4437aa05a7a27dc3803e0bd9f6b3d9eb2
Fernando Sanchez <perracolabs@gmail.com>
2023-08-27 11:53:52 +0200

    Rewritten EmployeeController with a more kotlin idiomatic style

--- src/main/kotlin/com/kcrud/controllers/EmployeeController.kt
+++ src/main/kotlin/com/kcrud/controllers/EmployeeController.kt
@@ -17,20 +17,11 @@ class EmployeeController(private val service: EmployeeService) : KoinComponent {
      * Responds with the employee details if found, otherwise appropriate error messages.
      */
     suspend fun get(call: ApplicationCall) {
-        val employeeId = call.parameters["id"]?.toIntOrNull()
-
-        if (employeeId == null) {
-            call.respond(HttpStatusCode.BadRequest, "Invalid employee ID.")
-            return
-        }
-
-        val employee = service.findById(employeeId)
-
-        if (employee == null) {
-            call.respond(HttpStatusCode.NotFound, "Employee ID not found: $employeeId")
-        } else {
-            call.respond(employee)
-        }
+        call.parameters["id"]?.toIntOrNull()?.let { employeeId ->
+            service.findById(employeeId)?.also { employee ->
+                call.respond(employee)
+            } ?: call.respond(HttpStatusCode.NotFound, "Employee ID not found: $employeeId")
+        } ?: call.respond(HttpStatusCode.BadRequest, "Invalid employee ID.")
     }
 
     /**
@@ -38,8 +29,7 @@ class EmployeeController(private val service: EmployeeService) : KoinComponent {
      * Responds with a list of all employees.
      */
     suspend fun getAll(call: ApplicationCall) {
-        val employees = service.findAll()
-        call.respond(employees)
+        call.respond(service.findAll())
     }
 
     /**
@@ -47,9 +37,9 @@ class EmployeeController(private val service: EmployeeService) : KoinComponent {
      * Reads the employee details from the request, creates a new employee, and responds with the created employee details.
      */
     suspend fun create(call: ApplicationCall) {
-        val employee = call.receive<EmployeeEntityIn>()
-        val newEmployee = service.create(employee)
-        call.respond(HttpStatusCode.Created, newEmployee)
+        call.receive<EmployeeEntityIn>()
+            .run { service.create(this) }
+            .also { newEmployee -> call.respond(HttpStatusCode.Created, newEmployee) }
     }
 
     /**
@@ -57,43 +47,33 @@ class EmployeeController(private val service: EmployeeService) : KoinComponent {
      * Reads the updated employee details from the request, updates the employee, and responds with the updated employee details.
      */
     suspend fun update(call: ApplicationCall) {
-        val employeeId = call.parameters["id"]?.toIntOrNull()
+        call.parameters["id"]?.toIntOrNull()?.let { employeeId ->
+            val updatedEmployee = call.receive<EmployeeEntityIn>().run {
+                service.update(employeeId, this)
+            }
 
-        if (employeeId == null) {
-            call.respond(HttpStatusCode.BadRequest, "Invalid employee ID.")
-            return
-        }
+            updatedEmployee?.also { employee ->
+                call.respond(HttpStatusCode.OK, employee)
+            } ?: call.respond(HttpStatusCode.NotFound, "Employee ID not found: $employeeId")
 
-        val employeeData = call.receive<EmployeeEntityIn>()
-        val updatedEmployee = service.update(employeeId, employeeData)
-
-        if (updatedEmployee == null) {
-            call.respond(HttpStatusCode.NotFound, "Employee ID not found: $employeeId")
-        } else {
-            call.respond(HttpStatusCode.OK, updatedEmployee)
-        }
+        } ?: call.respond(HttpStatusCode.BadRequest, "Invalid employee ID.")
     }
+
 
     /**
      * Handles PATCH request to update details of a specific employee.
      * Reads the updated employee details from the request, updates the employee, and responds with the updated employee details.
      */
     suspend fun patch(call: ApplicationCall) {
-        val employeeId = call.parameters["id"]?.toIntOrNull()
+        call.parameters["id"]?.toIntOrNull()?.let { employeeId ->
+            val patchedEmployee = call.receive<EmployeePatchDTO>().run {
+                service.patch(employeeId, this)
+            }
 
-        if (employeeId == null) {
-            call.respond(HttpStatusCode.BadRequest, "Invalid employee ID.")
-            return
-        }
-
-        val employeePatch = call.receive<EmployeePatchDTO>()
-        val patchedEmployee = service.patch(employeeId, employeePatch)
-
-        if (patchedEmployee == null) {
-            call.respond(HttpStatusCode.NotFound, "Employee ID not found: $employeeId")
-        } else {
-            call.respond(HttpStatusCode.OK, patchedEmployee)
-        }
+            patchedEmployee?.also { employee ->
+                call.respond(HttpStatusCode.OK, employee)
+            } ?: call.respond(HttpStatusCode.NotFound, "Employee ID not found: $employeeId")
+        } ?: call.respond(HttpStatusCode.BadRequest, "Invalid employee ID.")
     }
 
     /**
@@ -101,23 +81,18 @@ class EmployeeController(private val service: EmployeeService) : KoinComponent {
      * Deletes the employee if found and responds with appropriate status code.
      */
     suspend fun delete(call: ApplicationCall) {
-        val employeeId = call.parameters["id"]?.toIntOrNull()
-
-        if (employeeId == null) {
-            call.respond(HttpStatusCode.BadRequest, "Invalid employee ID.")
-            return
-        }
-
-        service.delete(employeeId)
-        call.respond(HttpStatusCode.NoContent)
+        call.parameters["id"]?.toIntOrNull()?.let { employeeId ->
+            service.delete(employeeId)
+            call.respond(HttpStatusCode.NoContent)
+        } ?: call.respond(HttpStatusCode.BadRequest, "Invalid employee ID.")
     }
+
 
     /**
      * Handles DELETE request to delete all employees.
      * Deletes all employees from the database and responds with an appropriate status code.
      */
     suspend fun deleteAll(call: ApplicationCall) {
-        service.deleteAll()
-        call.respond(HttpStatusCode.NoContent)
+        service.deleteAll().also { call.respond(HttpStatusCode.NoContent) }
     }
 }
