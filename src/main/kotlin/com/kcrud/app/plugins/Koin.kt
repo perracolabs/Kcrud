f3ebf673f7cd41715440b323c9eedfefc132f0eb
Fernando Sanchez <perracolabs@gmail.com>
2023-08-31 18:05:39 +0200

    Changed koin objects instantiation

--- src/main/kotlin/com/kcrud/app/plugins/Koin.kt
+++ src/main/kotlin/com/kcrud/app/plugins/Koin.kt
@@ -5,8 +5,6 @@ import com.kcrud.data.repositories.EmployeeRepository
 import com.kcrud.data.repositories.IEmployeeRepository
 import com.kcrud.services.EmployeeService
 import io.ktor.server.application.*
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
@@ -23,13 +21,13 @@ fun Application.configureDependencyInjection() {
 
         // Bind EmployeeRepository to the IEmployeeRepository interface.
         // Create a single instance of EmployeeRepository when an IEmployeeRepository is requested.
-        singleOf(::EmployeeRepository) { bind<IEmployeeRepository>() }
+        single<IEmployeeRepository> { EmployeeRepository() }
 
         // Create a single instance of EmployeeService.
-        singleOf(::EmployeeService)
+        single { EmployeeService(get()) }
 
         // Create a single instance of EmployeeController.
-        singleOf(::EmployeeController)
+        single { EmployeeController(get()) }
     }
 
     // Install the Koin DI framework and load the defined module.
