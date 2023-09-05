7db7bc60d8540c5575e78f9bb26156c8e79bc357
Fernando Sanchez <perracolabs@gmail.com>
2023-09-05 11:52:21 +0200

    Made authorization token resolver case-insensitive

--- src/main/kotlin/com/kcrud/utils/Security.kt
+++ src/main/kotlin/com/kcrud/utils/Security.kt
@@ -7,6 +7,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 
 /**
  * Security class responsible for verifying JWT tokens.
@@ -15,12 +16,8 @@ import kotlinx.coroutines.runBlocking
  * If the token is invalid or any exception occurs, an UnauthorizedException will be thrown.
  */
 class Security {
+    private val logger = LoggerFactory.getLogger(javaClass.simpleName)
 
-    /**
-     * Verify the JWT token from the incoming request.
-     *
-     * @param call The ApplicationCall from the incoming request.
-     */
     fun verifyToken(call: ApplicationCall) {
         val settings = call.application.appSettings()
 
@@ -28,21 +25,30 @@ class Security {
             return
 
         try {
+            val token = getAuthorizationToke(call)
             val algorithm: Algorithm = Algorithm.HMAC256(settings.jwt.secretKey)
             val verifier: JWTVerifier = JWT.require(algorithm).build()
-
-            val authHeader = call.request.headers.entries().find {
-                it.key.equals("Authorization", ignoreCase = true)
-            }?.value?.get(0) ?: ""
-
-            val token = authHeader.split("Bearer ").last()
             verifier.verify(JWT.decode(token))
         } catch (e: Exception) {
             println(e.message)
+            logger.error("Unauthorized: Error while authorizing.", e)
 
             runBlocking {
                 call.respond(HttpStatusCode.Unauthorized, "Unauthorized: Error while authorizing.")
             }
         }
+    }
+
+    private fun getAuthorizationToke(call: ApplicationCall): String {
+        val authHeader = call.request.headers.entries().find {
+            it.key.equals("Authorization", ignoreCase = true)
+        }?.value?.get(0) ?: ""
+
+        val bearerIndex = authHeader.indexOf("Bearer ", ignoreCase = true)
+        if (bearerIndex == -1) {
+            throw IllegalArgumentException("Missing Bearer in Authorization header.")
+        }
+
+        return authHeader.substring(bearerIndex + "Bearer ".length)
     }
 }
