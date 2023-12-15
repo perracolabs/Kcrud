cff87935dfcefb93e89c25dd9f4948067afc44d6
Fernando Sanchez <perracolabs@gmail.com>
2023-12-15 20:52:59 +0100

    Inject services to GraphQL configurator

---
+++ src/main/kotlin/com/kcrud/graphql/kgraphql/KGraphQLAPI.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 Perraco Labs. All rights reserved.
+ * This work is licensed under the terms of the MIT license.
+ * For a copy, see <https://opensource.org/licenses/MIT>
+ */
+
+package com.kcrud.graphql.kgraphql
+
+/**
+ * Annotation designed for controlled access to the KGraphQL management code,
+ * offering a workaround for the lack of package-private visibility in Kotlin.
+ */
+@RequiresOptIn(level = RequiresOptIn.Level.ERROR, message = "Only to be used in Expedia KGraphQL contexts.")
+@Retention(AnnotationRetention.BINARY)
+annotation class KGraphQLAPI
