6823c489b4f9a364e4a4fbf10f81fcdf1e3e7fdf
Fernando Sanchez <perracolabs@gmail.com>
2024-01-03 01:22:32 +0100

    Cleanup

--- src/main/kotlin/com/kcrud/api/graphql/frameworks/expedia/schema/employee/EmployeeQueries.kt
+++ src/main/kotlin/com/kcrud/api/graphql/frameworks/expedia/schema/employee/EmployeeQueries.kt
@@ -33,7 +33,8 @@ class EmployeeQueries : Query {
 
     @GraphQLDescription("Returns an employee given its id.")
     suspend fun employee(
-        @GraphQLDescription("The target employee to be returned.") employeeId: UUID
+        @GraphQLDescription("The target employee to be returned.")
+        employeeId: UUID
     ): Employee? {
         return service.findById(employeeId = employeeId)
     }
@@ -41,7 +42,8 @@ class EmployeeQueries : Query {
     @GraphQLDescription("Returns all existing employees.")
     suspend fun employees(
         env: DataFetchingEnvironment, // 'env' is internal, used to manage the query context, so no description is needed.
-        @GraphQLDescription("The pagination options to be applied. If not provided, a single page with the result will be returned.") pageable: Pageable? = null
+        @GraphQLDescription("Pagination options. If not provided, a single page is returned.")
+        pageable: Pageable? = null
     ): EmployeeConnection {
         // Example of how to get the user from the context.
         // See ContextFactory and SessionContext for more details.
@@ -49,15 +51,16 @@ class EmployeeQueries : Query {
 
         val page: Page<Employee> = service.findAll(pageable = pageable)
         return EmployeeConnection(
-            content = page.content,
-            info = page.info
+            content = page.content, info = page.info
         )
     }
 
     @GraphQLDescription("Filterable paginated Employee query.")
     suspend fun filterEmployees(
-        @GraphQLDescription("The filter set options to be applied.") filterSet: EmployeeFilterSet,
-        @GraphQLDescription("The pagination options to be applied. If not provided, a single page with the result will be returned.") pageable: Pageable? = null
+        @GraphQLDescription("Filter set options to be applied.")
+        filterSet: EmployeeFilterSet,
+        @GraphQLDescription("Pagination options. If not provided, a single page is returned.")
+        pageable: Pageable? = null
     ): EmployeeConnection {
         val page = service.filter(filterSet = filterSet, pageable = pageable)
         return EmployeeConnection(
