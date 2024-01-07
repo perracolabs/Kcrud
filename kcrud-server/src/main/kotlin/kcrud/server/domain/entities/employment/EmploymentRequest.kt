f5d55c0f2b737699cc0dbf3c70887aeeb9372d39
Fernando Sanchez <perracolabs@gmail.com>
2024-01-07 19:31:39 +0100

    Cleanup.

--- kcrud-server/src/main/kotlin/kcrud/server/domain/entities/employment/EmploymentRequest.kt
+++ kcrud-server/src/main/kotlin/kcrud/server/domain/entities/employment/EmploymentRequest.kt
@@ -8,6 +8,7 @@ package kcrud.server.domain.entities.employment
 
 import kcrud.server.domain.entities.employment.types.WorkModality
 import kcrud.server.domain.entities.shared.Period
+import kcrud.server.domain.exceptions.EmploymentError
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
 
@@ -23,18 +24,24 @@ data class EmploymentRequest(
     val probationEndDate: LocalDate? = null,
     val workModality: WorkModality
 ) {
-    // Example of a validation rule within a data class.
-    // This is not a good practice, as it couples the data class with the validation logic.
-    //
-    //    init {
-    //        probationEndDate?.let { date ->
-    //            require(date >= period.startDate) {
-    //                "Employment probation end date cannot be earlier than period start date."
-    //            }
-    //        }
-    //    }
-    //
-    // In addition to that, it is not possible to pass to the client more detailed information
-    // about the error, such as the field name or concrete code for the error.
-    // For a better approach, see the validation use of 'EmploymentError.PeriodDatesMismatch'.
+    /**
+     * Example of a validation rule within a data class.
+     * This is not a good practice, as it couples the data class with the validation logic.
+     *
+     * ```
+     *      init {
+     *          probationEndDate?.let { date ->
+     *              require(date >= period.startDate) {
+     *                  "Employment probation end date cannot be earlier than period start date."
+     *              }
+     *          }
+     *      }
+     * ```
+     *
+     * Such approach, other than an error message, does not allow to pass to the client
+     * more contextual information about the error, such as the field name decoupled from
+     * the message, and/or a more concrete error code which may reflect how the error happened,
+     * for example whether creating or updating, and other details such as the record IDs, etc.
+     * For a better approach, see the usage of [EmploymentError.PeriodDatesMismatch].
+     */
 }
