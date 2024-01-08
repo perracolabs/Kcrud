1d823b76c2af4e2f8a6ac1b50791591e97351046
Fernando Sanchez <perracolabs@gmail.com>
2024-01-08 19:32:54 +0100

    Updated dependencies

--- kcrud-server/src/main/kotlin/kcrud/server/domain/entities/contact/ContactRequest.kt
+++ kcrud-server/src/main/kotlin/kcrud/server/domain/entities/contact/ContactRequest.kt
@@ -7,10 +7,18 @@
 package kcrud.server.domain.entities.contact
 
 import kcrud.base.data.serializers.EmailString
+import kcrud.server.domain.exceptions.EmployeeError
 import kotlinx.serialization.Serializable
 
 /**
  * Represents the request to create/update an employee's contact details.
+ *
+ * This entity serves as example of how to use the [EmailString],
+ * which is a typealias for a serializable String that must be a valid email.
+ * Note that the project also includes examples about how perform verifications
+ * at service level, instead of using serializers.
+ * Such validators can send to the client a more detailed error than would
+ * do a serializer. See: [EmployeeError].
  *
  * @property email The contact's email. Must not be blank.
  * @property phone The contact's phone. Must not be blank.
--- kcrud-server/src/main/kotlin/kcrud/server/domain/entities/employee/EmployeeRequest.kt
+++ kcrud-server/src/main/kotlin/kcrud/server/domain/entities/employee/EmployeeRequest.kt
@@ -10,11 +10,19 @@ import kcrud.base.data.serializers.NoBlankString
 import kcrud.server.domain.entities.contact.ContactRequest
 import kcrud.server.domain.entities.employee.types.Honorific
 import kcrud.server.domain.entities.employee.types.MaritalStatus
+import kcrud.server.domain.exceptions.EmployeeError
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
 
 /**
  * Represents the request to create/update an employee.
+ *
+ * This entity serves as example of how to use the [NoBlankString],
+ * which is a typealias for a serializable String that cannot be blank.
+ * Note that the project also includes examples about how perform verifications
+ * at service level, instead of using serializers.
+ * Such validators can send to the client a more detailed error than would
+ * do a serializer. See: [EmployeeError].
  *
  * @property firstName The first name of the employee. Must not be blank.
  * @property lastName The last name of the employee. Must not be blank.
